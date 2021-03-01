8ac101fcfd1c66dc7a531ec0a9128fe790db9933
Lara Pruna Ternero <laraprn.12@gmail.com>
2021-03-01 13:29:35 +0100

    He modificado el fichero programa_json.py

--- programa_json.py
+++ programa_json.py
@@ -1,1 +1,30 @@
-from funciones_json import LeerJson 
+from funciones_json import LeerJson
+opcion=input('''Elige una opción del siguiente menú:
+1. Lista de películas
+2. Número de películas por categoría
+3. Buscar películas por intervalo de fechas
+4. Buscar películas de un actor en concreto
+5. Buscar películas por actor y categoría con tráiler disponible
+6. Salir''')
+while opcion<1 and opcion>6:
+	print("Error. Introduce el número de la opción correcta.")
+	opcion=input('''Elige una opción del siguiente menú:
+1. Lista de películas
+2. Número de películas por categoría
+3. Buscar películas por intervalo de fechas
+4. Buscar películas de un actor en concreto
+5. Buscar películas por actor y categoría con tráiler disponible
+6. Salir''')
+while opcion!=6:
+	if opcion==1:
+	elif opcion==2:
+	elif opcion==3:
+	elif opcion==4:
+	else:
+	opcion=input('''Elige una opción del siguiente menú:
+1. Lista de películas
+2. Número de películas por categoría
+3. Buscar películas por intervalo de fechas
+4. Buscar películas de un actor en concreto
+5. Buscar películas por actor y categoría con tráiler disponible
+6. Salir''')
