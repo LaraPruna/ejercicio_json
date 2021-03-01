d27292f8c479f5cccefa06752bade4a73721afa9
Lara Pruna Ternero <laraprn.12@gmail.com>
2021-03-01 13:45:04 +0100

    He añadido una nueva función al fichero funciones_json.py

--- funciones_json.py
+++ funciones_json.py
@@ -7,3 +7,11 @@ def LeerJson():
 		return datos
 	except:
 		print("Error al cargar el fichero.")
+
+def ListaPelis():
+	for cat in LeerJson().get("categorias"):
+		for pel in cat.get("playlist"):
+			print(f'''Título: {pel.get("titulo")}
+Año de estreno: {pel.get("anio")}''')
+			print()
+
--- programa_json.py
+++ programa_json.py
@@ -1,30 +1,36 @@
-from funciones_json import LeerJson
-opcion=input('''Elige una opción del siguiente menú:
+from funciones_json import LeerJson,ListaPelis
+opcion=int(input('''Elige una opción del siguiente menú:
+
 1. Lista de películas
 2. Número de películas por categoría
 3. Buscar películas por intervalo de fechas
 4. Buscar películas de un actor en concreto
 5. Buscar películas por actor y categoría con tráiler disponible
-6. Salir''')
+6. Salir
+
+Opción: '''))
 while opcion<1 and opcion>6:
 	print("Error. Introduce el número de la opción correcta.")
-	opcion=input('''Elige una opción del siguiente menú:
+	opcion=int(input('''Elige una opción del siguiente menú:
+
 1. Lista de películas
 2. Número de películas por categoría
 3. Buscar películas por intervalo de fechas
 4. Buscar películas de un actor en concreto
 5. Buscar películas por actor y categoría con tráiler disponible
-6. Salir''')
+6. Salir
+
+Opción: '''))
 while opcion!=6:
 	if opcion==1:
-	elif opcion==2:
-	elif opcion==3:
-	elif opcion==4:
-	else:
-	opcion=input('''Elige una opción del siguiente menú:
+		print(ListaPelis())
+	opcion=int(input('''Elige una opción del siguiente menú:
+
 1. Lista de películas
 2. Número de películas por categoría
 3. Buscar películas por intervalo de fechas
 4. Buscar películas de un actor en concreto
 5. Buscar películas por actor y categoría con tráiler disponible
-6. Salir''')
+6. Salir
+
+Opción: '''))
